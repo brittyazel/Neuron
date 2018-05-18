733e4049d059549ed8a8055ab7537ecb233b96fc
Britt Yazel <bwyazel@gmail.com>
2018-05-17 19:41:27 -0700

    move interface panel options to NeuronGUI

--- Neuron-GUI.lua
+++ Neuron-GUI.lua
@@ -104,6 +104,12 @@ function NeuronGUI:OnInitialize()
 
 	MAS = NEURON.MANAGED_ACTION_STATES
 
+	---This loads the Neuron interface panel
+	LibStub("AceConfigRegistry-3.0"):ValidateOptionsTable(NeuronGUI.interfaceOptions, addonName)
+	LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, NeuronGUI.interfaceOptions)
+	NeuronGUI.interfaceOptions.args.profile = LibStub("AceDBOptions-3.0"):GetOptionsTable(NEURON.db)
+	LibStub("AceConfigDialog-3.0"):AddToBlizOptions(addonName, addonName)
+
 
 end
 
@@ -3881,4 +3887,133 @@ NeuronGUI.flyout_options = {
 	},
 
 
+}
+
+
+
+
+--ACE GUI OPTION TABLE
+NeuronGUI.interfaceOptions = {
+	name = "Neuron",
+	type = 'group',
+	args = {
+		moreoptions={
+			name = L["Options"],
+			type = "group",
+			order = 0,
+			args={
+				BlizzardBar = {
+					order = 1,
+					name = L["Display the Blizzard Bar"],
+					desc = L["Shows / Hides the Default Blizzard Bar"],
+					type = "toggle",
+					set = function() NEURON:BlizzBar() end,
+					get = function() return NeuronGDB.mainbar end,
+					width = "full",
+				},
+				NeuronMinimapButton = {
+					order = 2,
+					name = L["Display Minimap Button"],
+					desc = L["Toggles the minimap button."],
+					type = "toggle",
+					set =  function() NEURON.NeuronMinimapIcon:ToggleIcon() end,
+					get = function() return not NeuronGDB.NeuronIcon.hide end,
+					width = "full"
+				},
+			},
+		},
+
+		changelog = {
+			name = L["Changelog"],
+			type = "group",
+			order = 1000,
+			args = {
+				line1 = {
+					type = "description",
+					name = L["Changelog_Latest_Version"],
+				},
+			},
+		},
+
+		faq = {
+			name = L["F.A.Q."],
+			desc = L["Frequently Asked Questions"],
+			type = "group",
+			order = 1001,
+			args = {
+
+				line1 = {
+					type = "description",
+					name = L["FAQ_Intro"],
+				},
+
+				g1 = {
+					type = "group",
+					name = L["Bar Configuration"],
+					order = 1,
+					args = {
+
+						line1 = {
+							type = "description",
+							name = L["Bar_Configuration_FAQ"],
+							order = 1,
+						},
+
+						g1 = {
+							type = "group",
+							name = L["General Options"],
+							order = 1,
+							args = {
+								line1 = {
+									type = "description",
+									name = L["General_Bar_Configuration_Option_FAQ"] ,
+									order = 1,
+								},
+							},
+						},
+
+						g2 = {
+							type = "group",
+							name = L["Bar States"],
+							order = 2,
+							args = {
+								line1 = {
+									type = "description",
+									name = L["Bar_State_Configuration_FAQ"],
+									order = 1,
+								},
+							},
+						},
+
+						g3 = {
+							type = "group",
+							name = L["Spell Target Options"],
+							order = 3,
+							args = {
+								line1 = {
+									type = "description",
+									name = L["Spell_Target_Options_FAQ"],
+									order = 1,
+								},
+							},
+						},
+					},
+				},
+
+				g2 = {
+					type = "group",
+					name = L["Flyout"],
+					order = 3,
+					args = {
+						line1a = {
+							type = "description",
+							name = L["Flyout_FAQ"],
+							order = 1,
+						},
+					},
+				},
+
+			},
+		},
+	},
 }
