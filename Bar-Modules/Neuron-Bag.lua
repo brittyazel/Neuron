0f38c45fc40ded82d2f0d1e487bd35d78a08001b
Britt Yazel <bwyazel@gmail.com>
2018-06-03 12:51:10 -0700

    rewrite bag bar completely to piggyback off blizzard code

--- Bar-Modules/Neuron-Bag.lua
+++ Bar-Modules/Neuron-Bag.lua
@@ -44,22 +44,11 @@ local configData = {
 --- or setting up slash commands.
 function NeuronBagBar:OnInitialize()
 
-	bagElements[1] = NeuronBackpackButton
-	bagElements[2] = Neuron___Bag0Slot
-	bagElements[3] = Neuron___Bag1Slot
-	bagElements[4] = Neuron___Bag2Slot
-	bagElements[5] = Neuron___Bag3Slot
-
-	for k,v in pairs(bagElements) do
-		v:SetWidth(32)
-		v:SetHeight(32)
-		v:GetNormalTexture():SetWidth(55)
-		v:GetNormalTexture():SetHeight(55)
-		v:GetNormalTexture():SetPoint("CENTER",0,0)
-		_G[v:GetName().."IconTexture"]:ClearAllPoints()
-		_G[v:GetName().."IconTexture"]:SetPoint("TOPLEFT", -1, 1)
-		_G[v:GetName().."IconTexture"]:SetPoint("BOTTOMRIGHT")
-	end
+	bagElements[1] = MainMenuBarBackpackButton
+	bagElements[2] = CharacterBag0Slot
+	bagElements[3] = CharacterBag1Slot
+	bagElements[4] = CharacterBag2Slot
+	bagElements[5] = CharacterBag3Slot
 
 	DB = NeuronCDB
 
@@ -99,16 +88,6 @@ end
 --- the game that wasn't available in OnInitialize
 function NeuronBagBar:OnEnable()
 
-	NeuronBagBar:SecureHook("ContainerFrame_OnShow", function(self) NeuronBagBar:containerFrame_OnShow(self) end)
-	NeuronBagBar:SecureHook("ContainerFrame_OnHide", function(self) NeuronBagBar:containerFrame_OnHide(self) end)
-	NeuronBagBar:SecureHook("ToggleBag", function(self, id) NeuronBagBar:toggleBag(id) end)
-	NeuronBagBar:SecureHook("ToggleBackpack", function(self, id) NeuronBagBar:toggleBackpack(id) end)
-
-	for i=1,13 do
-		local frame = _G["ContainerFrame"..i]
-		NeuronBagBar:HookScript(frame, "OnShow", function(self) NeuronBagBar:containerFrame_OnShow(self) end)
-		NeuronBagBar:HookScript(frame, "OnHide", function(self) NeuronBagBar:containerFrame_OnHide(self) end)
-	end
 
 end
 
@@ -157,181 +136,6 @@ function NeuronBagBar:CreateBarsAndButtons()
 end
 
 
-
-
-function NeuronBagBar:toggleBag(id)
-
-	if (not InCombatLockdown() and IsOptionFrameOpen()) then
-
-		local size = GetContainerNumSlots(id)
-		if (size > 0 or id == KEYRING_CONTAINER) then
-			local containerShowing;
-			for i=1, NUM_CONTAINER_FRAMES, 1 do
-				local frame = _G["ContainerFrame"..i]
-				if (frame:IsShown() and frame:GetID() == id) then
-					containerShowing = i
-					frame:Hide()
-				end
-			end
-			if (not containerShowing) then
-				ContainerFrame_GenerateFrame(ContainerFrame_GetOpenFrame(), size, id)
-			end
-		end
-	end
-end
-
-function NeuronBagBar:toggleBackpack()
-
-	if (not InCombatLockdown() and IsOptionFrameOpen()) then
-
-		if (IsBagOpen(0)) then
-			for i=1, NUM_CONTAINER_FRAMES, 1 do
-				local frame = _G["ContainerFrame"..i]
-				if (frame:IsShown()) then
-					frame:Hide()
-				end
-				-- Hide the token bar if closing the backpack
-				if (BackpackTokenFrame) then
-					BackpackTokenFrame:Hide()
-				end
-			end
-		else
-			ToggleBag(0)
-			-- If there are tokens watched then show the bar
-			if (ManageBackpackTokenFrame) then
-				BackpackTokenFrame_Update()
-				ManageBackpackTokenFrame()
-			end
-		end
-	end
-end
-
-function NeuronBagBar:containerFrame_OnShow(frame)
-
-	local index = frame:GetID() + 1
-
-	if (bagElements[index]) then
-		bagElements[index]:SetChecked(1)
-	end
-end
-
-function NeuronBagBar:containerFrame_OnHide(frame)
-
-	local index = abs(frame:GetID()-5)
-
-	if (bagElements[index]) then
-		bagElements[index]:SetChecked(0)
-	end
-end
-
-function NeuronBagBar:updateFreeSlots(bag)
-
-	local totalSlots, totalFree  = 0, 0
-	local freeSlots, bagFamily
-
-	for i=BACKPACK_CONTAINER, NUM_BAG_SLOTS do
-
-		freeSlots, bagFamily = GetContainerNumFreeSlots(i)
-
-		if (bagFamily == 0) then
-
-			totalSlots = totalSlots + GetContainerNumSlots(i)
-			totalFree = totalFree + freeSlots
-		end
-	end
-
-	local rgbValue, r, g
-	if (freeSlots > 0) then
-		rgbValue = math.floor((totalFree/freeSlots)*100)
-	else
-		rgbValue = 1
-	end
-
-	if (rgbValue > 49) then
-		r=(1-(rgbValue/100))+(1-(rgbValue/100))
-		g=(rgbValue/100)+((1-(rgbValue/100))/2)
-	else
-		r=1; g=(rgbValue/100)*1.5
-	end
-
-	bag.freeSlots = totalFree
-
-	bag.count:SetText(string.format("%s", totalFree))
-	bag.count:SetTextColor(r, g, 0)
-end
-
-function NeuronBagBar:NeuronBackpackButton_OnLoad(frame)
-
-	frame:RegisterForClicks("LeftButtonUp", "RightButtonUp")
-	frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-	frame:RegisterEvent("CVAR_UPDATE")
-	frame:RegisterEvent("BAG_UPDATE")
-
-	frame.icon:SetTexture("Interface\\Buttons\\Button-Backpack-Up")
-	--self.BlizzBP = MainMenuBarBackpackButton
-
-	frame.count = _G[frame:GetName().."Count2"]
-	frame.icon = _G[frame:GetName().."IconTexture"]
-end
-
-function NeuronBagBar:NeuronBackpackButton_OnReceiveDrag(frame, button)
-
-	if (not PutItemInBackpack()) then
-		ToggleBackpack()
-	end
-
-	local isVisible
-
-	for i=1, NUM_CONTAINER_FRAMES, 1 do
-		local frame = _G["ContainerFrame"..i]
-
-		if (frame:GetID()==0 and frame:IsShown()) then
-			isVisible = 1; break
-		end
-	end
-
-	frame:SetChecked(isVisible)
-end
-
-function NeuronBagBar:NeuronBackpackButton_OnEnter(frame)
-
-	GameTooltip:SetOwner(frame, "ANCHOR_LEFT")
-	GameTooltip:SetText(BACKPACK_TOOLTIP, 1.0, 1.0, 1.0)
-
-	local keyBinding = GetBindingKey("TOGGLEBACKPACK")
-
-	if (keyBinding) then
-		GameTooltip:AppendText(" "..NORMAL_FONT_COLOR_CODE.."("..keyBinding..")"..FONT_COLOR_CODE_CLOSE)
-	end
-
-	GameTooltip:AddLine(string.format(NUM_FREE_SLOTS, (frame.freeSlots or 0)))
-
-	GameTooltip:Show()
-end
-
-function NeuronBagBar:NeuronBackpackButton_OnLeave(frame)
-	GameTooltip:Hide()
-end
-
-function NeuronBagBar:NeuronBackpackButton_OnEvent(frame, event, ...)
-
-	if (event == "BAG_UPDATE") then
-
-		if (... >= BACKPACK_CONTAINER and ... <= NUM_BAG_SLOTS) then
-			NeuronBagBar:updateFreeSlots(frame)
-		end
-
-	elseif (event == "PLAYER_ENTERING_WORLD") then
-
-		NeuronBagBar:updateFreeSlots(frame)
-
-		frame.icon:SetTexture([[Interface\Buttons\Button-Backpack-Up]])
-
-	elseif (event == "CVAR_UPDATE") then
-
-	end
-end
-
 function NeuronBagBar:SetSkinned(button)
 
 	if (SKIN) then
@@ -363,14 +167,6 @@ function NeuronBagBar:SetData(button, bar)
 		button:SetFrameStrata(bar.gdata.objectStrata)
 		button:SetScale(bar.gdata.scale)
 
-		if (button.element == NeuronBackpackButton) then
-
-			if (bar.objCount == 1) then
-				button.element:SetScript("OnClick", function() if (ContainerFrame1:IsVisible()) then CloseAllBags() else OpenAllBags() end end)
-			else
-				button.element:SetScript("OnClick", function() if (IsModifiedClick()) then if (ContainerFrame1:IsVisible()) then CloseAllBags() else OpenAllBags() end else ToggleBackpack() end end)
-			end
-		end
 	end
 
 	button:SetFrameLevel(4)
@@ -446,8 +242,14 @@ function NeuronBagBar:SetType(button, save)
 
 	if (bagElements[button.id]) then
 
-		button:SetWidth(bagElements[button.id]:GetWidth()+3)
-		button:SetHeight(bagElements[button.id]:GetHeight()+3)
+		if button.id == 1 then
+			button:SetWidth(bagElements[button.id]:GetWidth()-4)
+			button:SetHeight(bagElements[button.id]:GetHeight()-4)
+		else
+			button:SetWidth(bagElements[button.id]:GetWidth()+3)
+			button:SetHeight(bagElements[button.id]:GetHeight()+3)
+		end
+
 		button:SetHitRectInsets(button:GetWidth()/2, button:GetWidth()/2, button:GetHeight()/2, button:GetHeight()/2)
 
 		button.element = bagElements[button.id]
@@ -465,11 +267,6 @@ function NeuronBagBar:SetType(button, save)
 		button.element:SetPoint("CENTER", button, "CENTER")
 		button.element:SetScale(1)
 
-		if (button.element == NeuronBackpackButton) then
-			button.element:SetScript("OnClick", function() if (IsModifiedClick()) then if (ContainerFrame1:IsVisible()) then CloseAllBags() else OpenAllBags() end else ToggleBackpack() end end)
-		else
-			button.element:SetScript("OnClick", BagSlotButton_OnClick)
-		end
 
 		button:SetSkinned(button)
 	end
