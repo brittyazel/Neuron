7aea7e8b0e0bebd8fb84ff0ada33a7fe878e0f38
Britt Yazel <bwyazel@gmail.com>
2018-10-29 13:46:36 -0700

    fix #185

--- Neuron-Bars.lua
+++ Neuron-Bars.lua
@@ -1666,7 +1666,7 @@ function NeuronBar:LoadData(bar)
 
 	bar.data = bar.barDB[id]
 
-	if (#bar.data.name < 1) then
+	if (not bar.data.name) then
 		bar.data.name = bar.barLabel.." "..bar:GetID()
 	end
 end
