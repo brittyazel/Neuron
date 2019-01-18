4ec8e19f0debbc7700afc60faee8e467e45f70dd
Britt Yazel <bwyazel@gmail.com>
2019-01-17 19:16:41 -0800

    #260 try again to fix this

--- Objects/BUTTON.lua
+++ Objects/BUTTON.lua
@@ -427,9 +427,10 @@ function BUTTON:SetData(bar)
 		self:SetScale(bar.data.scale)
 	end
 
-	self:SetFrameLevel(4)
+	--self:SetFrameLevel(4)
 	self.iconframe:SetFrameLevel(2)
 	self.iconframecooldown:SetFrameLevel(3)
+	self.iconframechargecooldown:SetFrameLevel(3)
 
 
 	if (self.bindText) then
