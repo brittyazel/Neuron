b3fd2711e40a6bb0c9668d6dc5272311b780f655
Britt Yazel <bwyazel@gmail.com>
2019-09-23 20:11:28 -0700

    easier check for WoW Classic

--- Neuron.lua
+++ Neuron.lua
@@ -123,7 +123,9 @@ Neuron.SNAPTO_TOLLERANCE = 28
 
 Neuron.enteredWorld = false --flag that gets set when the player enters the world. It's used primarily for throttling events so that the player doesn't crash on logging with too many processes
 
-Neuron.isWoWClassic = select(4, GetBuildInfo()) < 20000
+if WOW_PROJECT_ID == WOW_PROJECT_CLASSIC then --boolean check to set a flag if the current session is WoW Classic. Retail == 1, Classic == 2
+	Neuron.isWoWClassic = true
+end
 
 Neuron.activeSpec = 1
 
