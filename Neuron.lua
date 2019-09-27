26735f8cc2d36d0d90d7dc739e1099e0080429ed
Britt Yazel <bwyazel@gmail.com>
2019-09-27 09:09:54 -0700

    fixes #319

--- Neuron.lua
+++ Neuron.lua
@@ -179,6 +179,9 @@ function Neuron:OnInitialize()
 	--Initialize the chat commands (i.e. /neuron)
 	Neuron:RegisterChatCommand("neuron", "slashHandler")
 
+	--build all bar and button frames and run initial setup
+	Neuron:Startup()
+
 end
 
 --- **OnEnable** which gets called during the PLAYER_LOGIN event, when most of the data provided by the game is already present.
@@ -229,9 +232,6 @@ function Neuron:OnEnable()
 	if not Neuron.isWoWClassic then
 		Neuron.activeSpec = GetSpecialization()
 	end
-
-	--build all bar and button frames and run initial setup
-	Neuron:Startup()
 
 	--Load all bars and buttons
 	for i,v in pairs(Neuron.BARIndex) do
