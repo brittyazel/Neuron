6f182753dfa39f54fd389ed53e8310620fc8489f
Britt Yazel <bwyazel@gmail.com>
2020-04-20 16:00:00 -0700

    fixes #346, Classic doesn't have toys

--- Objects/BUTTON.lua
+++ Objects/BUTTON.lua
@@ -768,7 +768,7 @@ function BUTTON:UpdateUsableItem()
 	--for some reason toys don't show as usable items, so this is a workaround for that
 	if not isUsable then
 		local itemID = GetItemInfoInstant(self.item)
-		if itemID and PlayerHasToy(itemID) then
+		if not Neuron.isWoWClassic and itemID and PlayerHasToy(itemID) then
 			isUsable = true
 		end
 	end
