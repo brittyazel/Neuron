2258101d69665cd91b35ec43da3f6bb379ee7793
Jon Allen <jon@ylixir.io>
2022-11-03 04:29:08 +0000

    DRY up default array initialization

--- Neuron-Defaults.lua
+++ Neuron-Defaults.lua
@@ -6,6 +6,8 @@
 local _, addonTable = ...
 local Neuron = addonTable.Neuron
 
+local Array = addonTable.utilities.Array
+
 Neuron.DefaultBarOptions = {}
 
 addonTable.defaultBarOptions = {}
@@ -27,44 +29,10 @@ addonTable.defaultBarOptions.ActionBar = {
 		possess = true,
 		override = true,
 
-		buttons = {
-			[1] = {
-				keys = { hotKeys = ":1:", hotKeyText = ":1:",},
-			},
-			[2] = {
-				keys = { hotKeys = ":2:", hotKeyText = ":2:",},
-			},
-			[3] = {
-				keys = { hotKeys = ":3:", hotKeyText = ":3:",},
-			},
-			[4] = {
-				keys = { hotKeys = ":4:", hotKeyText = ":4:",},
-			},
-			[5] = {
-				keys = { hotKeys = ":5:", hotKeyText = ":5:",},
-			},
-			[6] = {
-				keys = { hotKeys = ":6:", hotKeyText = ":6:",},
-			},
-			[7] = {
-				keys = { hotKeys = ":7:", hotKeyText = ":7:",},
-			},
-			[8] = {
-				keys = { hotKeys = ":8:", hotKeyText = ":8:",},
-			},
-			[9] = {
-				keys = { hotKeys = ":9:", hotKeyText = ":9:",},
-			},
-			[10] = {
-				keys = { hotKeys = ":0:", hotKeyText = ":0:",},
-			},
-			[11] = {
-				keys = { hotKeys = ":-:", hotKeyText = ":-:",},
-			},
-			[12] = {
-				keys = { hotKeys = ":=:", hotKeyText = ":=:",},
-			},
-		},
+		buttons = Array.map(
+			function(key) return { keys = { hotKeys = key, hotKeyText = key,}, } end,
+			{ ":1:", ":2:", ":3:", ":4:", ":5:", ":6:", ":7:", ":8:", ":9:", ":0:", ":-:", ":=:",}
+		),
 	},
 
 	[2] = {
@@ -76,20 +44,7 @@ addonTable.defaultBarOptions.ActionBar = {
 		y = 100,
 		showGrid = true,
 
-		buttons = {
-			[1] = {},
-			[2] = {},
-			[3] = {},
-			[4] = {},
-			[5] = {},
-			[6] = {},
-			[7] = {},
-			[8] = {},
-			[9] = {},
-			[10] = {},
-			[11] = {},
-			[12] = {},
-		},
+		buttons = Array.initialize(12, function() return {} end),
 	}
 }
 
@@ -108,11 +63,7 @@ addonTable.defaultBarOptions.ZoneAbilityBar = {
 		y = 75,
 		border = true,
 
-		buttons = {
-			[1] = {},
-			[2] = {},
-			[3] = {},
-		},
+		buttons = Array.initialize(3, function() return {} end),
 	}
 }
 
@@ -222,19 +173,10 @@ addonTable.defaultBarOptions.MirrorBar = {
 		x = 0,
 		y = -123,
 
-		buttons = {
-			[1] = {
-				config = {cIndex = 1, lIndex = 2, rIndex = 3},
-			},
-			[2] = {
-				config = {cIndex = 1, lIndex = 2, rIndex = 3},
-			},
-			[3] = {
-				config = {cIndex = 1, lIndex = 2, rIndex = 3},
-			},
-		},
-
-
+		buttons = Array.initialize(
+			3,
+			function() return { config = {cIndex = 1, lIndex = 2, rIndex = 3}, } end
+		),
 	}
 }
 
@@ -271,18 +213,7 @@ addonTable.defaultBarOptions.PetBar = {
 		x = -500,
 		y = 75,
 
-		buttons = {
-			[1] = {},
-			[2] = {},
-			[3] = {},
-			[4] = {},
-			[5] = {},
-			[6] = {},
-			[7] = {},
-			[8] = {},
-			[9] = {},
-			[10] = {},
-		},
+		buttons = Array.initialize(10, function() return {} end),
 	}
 }
 
@@ -303,19 +234,7 @@ if not Neuron.isWoWClassicEra and not Neuron.isWoWClassic then
 			padH = 0,
 			padV = 0,
 
-			buttons = {
-				[1] = {},
-				[2] = {},
-				[3] = {},
-				[4] = {},
-				[5] = {},
-				[6] = {},
-				[7] = {},
-				[8] = {},
-				[9] = {},
-				[10] = {},
-				[11] = {},
-			},
+			buttons = Array.initialize(11, function() return {} end),
 		}
 	}
 else
@@ -328,12 +247,9 @@ else
 			x = -370,
 			y = 33,
 
-			buttons = {},
+			buttons = Array.initialize(#MICRO_BUTTONS, function() return {} end),
 		}
 	}
-	for i = 1,#MICRO_BUTTONS do
-		addonTable.defaultBarOptions.MenuBar[1].buttons[i] = {}
-	end
 end
 
 -----------------------------------
@@ -348,10 +264,10 @@ if not Neuron.isWoWClassicEra and not Neuron.isWoWClassic then
 			snapToFrame = false,
 			snapToPoint = false,
 			point = "BOTTOMRIGHT",
-			x = -108,
+			x = -125,
 			y = 24,
 
-			buttons = {},
+			buttons = Array.initialize(Neuron.NUM_BAG_BUTTONS, function() return {} end),
 		}
 	}
 else
@@ -366,10 +282,7 @@ else
 			x = -133,
 			y = 24,
 
-			buttons = {},
+			buttons = Array.initialize(Neuron.NUM_BAG_BUTTONS, function() return {} end),
 		}
 	}
-end
-for i = 1,Neuron.NUM_BAG_BUTTONS do
-	addonTable.defaultBarOptions.BagBar[1].buttons[i] = {}
 end
